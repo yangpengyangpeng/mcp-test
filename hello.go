8e2bf0c9415a40a43d439239b924673bda0ef834
Steven Wilkin <stevenwilkin@gmail.com>
2014-11-26 23:47:32 +0000

    Add hello world app

---
+++ hello.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+)
+
+var (
+	port = flag.Int("p", 8080, "Port to listen on")
+)
+
+func init() {
+	flag.Parse()
+}
+
+func main() {
+	fmt.Printf("> Starting on http://0.0.0.0:%d\n", *port)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello Go!")
+	})
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
+	if err != nil {
+		fmt.Println("Error starting!")
+	}
+}
